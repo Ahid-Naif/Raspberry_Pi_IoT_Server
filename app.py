e7ffbdafabaeed7575273b0bd13d27eebffe9ff7
unknown <ahedqaood19@gmail.com>
2020-02-25 09:57:14 +0300

    add ther server

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import json
+import Adafruit_CharLCD as LCD
+from components.lcd import print_message
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+@app.route('/lcd_display', methods = ['POST'])
+def displayLCD():
+    data = request.get_json()
+    data = data['lcd_text']
+    lcd  = LCD.Adafruit_CharLCDBackpack(address=0x21)
+    print_message(lcd, data)
+   
+    return 'data was received successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
