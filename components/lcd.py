237907e31365ae2bfd61a4c2c09c69de6df41ddc
unknown <ahedqaood19@gmail.com>
2020-02-25 09:57:40 +0300

    add lcd related functions

---
+++ components/lcd.py
@@ -0,0 +1,21 @@
+# class lcd_I2C():
+#     def __init__(self):
+#         # Define LCD column and row size for 16x2 LCD.
+#         self.lcd_columns = 16
+#         self.lcd_rows    = 2
+
+#         # Initialize the LCD using the pins
+#         self.lcd = LCD.Adafruit_CharLCDBackpack(address=0x21)
+
+def print_message(lcd, message):
+    try:
+        lcd.clear()
+        # Turn backlight on
+        lcd.set_backlight(0)
+
+        lcd.message(message)
+
+    except KeyboardInterrupt:
+        # Turn the screen off
+        lcd.clear()
+        lcd.set_backlight(1)
