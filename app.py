9cad77509d4e6017d65ae486b06dac0f86957c31
unknown <ahedqaood19@gmail.com>
2020-03-01 20:35:59 +0300

    handle /buzzer_on & /buzzer_off POST requests

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import json
 import Adafruit_CharLCD as LCD
 from components.lcd import print_message, clear_lcd
+from components.buzzer import buzzer_on, buzzer_off
 
 app = Flask(__name__)
 
@@ -24,6 +25,18 @@ def clearLCD():
     clear_lcd(lcd)
    
     return 'LCD was cleared successfully'
+
+@app.route('/buzzer_on', methods = ['POST'])
+def buzzerON():
+    data = request.get_json()
+    data = data['status']
+    buzzer_on(data)
+
+@app.route('/buzzer_off', methods = ['POST'])
+def buzzerOFF():
+    data = request.get_json()
+    data = data['status']
+    buzzer_off(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
